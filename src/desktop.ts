89f386d01f8286e5ba6f2db95c63edaf9c50f26c
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-04-15 17:22:14 -0500

    Create desktop with task bar and funny microsoft logo

---
+++ src/desktop.ts
@@ -0,0 +1,52 @@
+import { Button } from './webtk/components/button.js';
+import { Image } from './webtk/components/image.js';
+import { Label } from './webtk/components/label.js';
+import { Window } from './webtk/components/window.js';
+
+import { Application } from './webtk/application.js';
+
+export class Desktop {
+	taskbar: Taskbar;
+
+	constructor() {
+
+		this.taskbar = new Taskbar();
+	}
+
+	show() {
+		this.taskbar.show();
+		document.getElementById('windows').appendChild(this.taskbar.element);
+	}
+}
+
+class Taskbar extends Window {
+	start: Startmenu;
+	startButton: Button;
+
+	constructor() {
+		super(new Application('taskbar'), false, false);
+
+		this.start = new Startmenu();
+
+		this.startButton = new Button((ev) => { this.start.visible ? this.start.hide() : this.start.show() });
+		this.startButton.addChild(new Image('http://www.pngmart.com/files/3/Windows-XP-PNG-Photos.png', 25, 25));
+		this.startButton.setWidth(25);
+		this.startButton.setHeight(25);
+		this.addChild(this.startButton);
+
+		document.getElementById('windows').appendChild(this.start.element);
+	}
+}
+
+class Startmenu extends Window {
+	startButton: Button;
+
+	constructor() {
+		super(new Application('startmenu'), false, false);
+
+		this.setPosition(0, 32);
+		this.hide();
+
+		this.addChild(new Label('Start Menu'));
+	}
+}
