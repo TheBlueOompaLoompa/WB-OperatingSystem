551f2dbc7a0d274900fcce9c8ac9ec16aeffdde6
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-04-16 02:41:17 -0500

    Add close button to header Update title change implementation to support buttons

--- src/webtk/components/header.ts
+++ src/webtk/components/header.ts
@@ -1,21 +1,51 @@
+import { Box } from './box.js';
+import { Button } from './button.js';
+import { Label } from './label.js';
 import { Widget } from './widget.js';
+import { Window } from './window.js';
 
 export class Header extends Widget {
 	title: string;
 	showControls: boolean = false;
-	constructor(uid: string, showControls: boolean) {
+	container: Box;
+	titleElement: Label;
+	parentWindow: Window;
+	
+	constructor(text: string, showControls: boolean, window: Window) {
 		super();
 
-		this.title = uid;
+		this.title = text;
 		this.showControls = showControls;
 
-		this.element.id = uid + 'header';
+		this.element.id = text + 'header';
 		this.element.className = 'header';
 
-		this.setTitle(uid);
+		this.container = new Box();
+		this.addChild(this.container);
+		this.container.setOrientation(false);
+
+		this.setTitle(text);
+
+		this.parentWindow = window;
+
+		this.setShowControls(showControls);
 	}
 
 	setTitle(title: string) {
-		this.element.innerHTML = title;
+		if(!this.titleElement) {
+			this.titleElement = new Label(title);
+			this.container.addChild(this.titleElement)
+		}else {
+			this.titleElement.setText(title);
+		}
+	}
+
+	setShowControls(show: boolean) {
+		// Create close button
+		var closeButton = new Button(() => {this.parentWindow.close()});
+		closeButton.element.innerText = '🮛';
+		closeButton.element.style.width = '25px';
+		closeButton.element.style.height = '25px';
+		this.container.addChild(closeButton);
 	}
 }
