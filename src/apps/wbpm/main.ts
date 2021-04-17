220064dc5172cb718c638a1ef358d2e052842633
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-04-17 13:57:20 -0500

    Allow adding new sources

--- src/apps/wbpm/main.ts
+++ src/apps/wbpm/main.ts
@@ -1,38 +1,44 @@
+async function doGET(url) {
+	return fetch(url);
+}
+
 export function main(Wbtk){
 	// Create app and window
 	var application = new Wbtk.Application('wbpm');
 	var window = new Wbtk.Window(application, true);
 	window.header.setTitle('Package Manager');
-	window.setWidth(400);
+	window.setWidth(700);
 
 	// Create package list view
 	let appsObject = JSON.parse(localStorage.getItem('apps'));
-	console.log(appsObject)
 	let apps = Object.keys(appsObject);
 
 	var appList = new Wbtk.ListView();
 	appList.justifyItems('start');
 
-	apps.forEach(app => {
-		console.log(app)
+	function addApp(path: string, name: string){ 
 		let listItem = new Wbtk.Box();
-		listItem.addChild(new Wbtk.Label(`${app} (<a href=${appsObject[app]}/meta.json>${appsObject[app]}</a>)`));
+		listItem.addChild(new Wbtk.Label(`${name} (<a href=${path}/meta.json>${path}</a>)`));
 		var removeButton = new Wbtk.Button(() => {
 			// Show popup
-			alert(`Source (${appsObject[app]}) has been uninstalled.\nWARNING: Uninstalling an app while it's running may leave your system unstable!`);
+			alert(`Source (${path}) has been uninstalled.\nWARNING: Uninstalling an app while it's running may leave your system unstable!`);
 
 			// Remove package from local storage
-			delete appsObject[app];
+			delete appsObject[name];
 			for(var i = 0; i < apps.length; i++){
-				if(app == apps[i]) apps.splice(i, 1);
+				if(name == apps[i]) apps.splice(i, 1);
 			}
 
 			appList.remove(listItem);
-			localStorage.setItem('apps', JSON.stringify(appList));
+			localStorage.setItem('apps', JSON.stringify(appsObject));
 		});
 		removeButton.addChild(new Wbtk.Label('Remove'));
 		listItem.addChild(removeButton)
 		appList.add(listItem);
+	}
+
+	apps.forEach(app => {
+		addApp(appsObject[app], app);
 	});
 
 	window.addChild(appList);
@@ -42,8 +48,15 @@ export function main(Wbtk){
 	window.addChild(urlInput);
 
 	// Create add button
-	var button = new Wbtk.Button(() => {
-		alert(`Source (${urlInput.getText()}) has been added to the sources list and will now be loaded.\nWARNING: Trying to use the app without restarting may leave your system unstable!`);
+	var button = new Wbtk.Button(async () => {
+		const url = urlInput.getText();
+		var response = await doGET(`${url}/meta.json`);
+		var val = await response.body.getReader().read();
+		var obj = JSON.parse(new TextDecoder().decode(val['value']));
+		appsObject[obj.name] = url;
+		addApp(url, obj.name);
+		localStorage.setItem('apps', JSON.stringify(appsObject));
+		alert(`Source (${url}) has been added to the sources list and will now be loaded.\nWARNING: Trying to use the app without restarting may leave your system unstable!`);
 	});
 	var center = new Wbtk.Box('center');
 	center.addChild(button);
