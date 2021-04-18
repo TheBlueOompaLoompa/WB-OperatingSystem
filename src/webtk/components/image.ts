35cf2c97ba4d5be79668c5c8674fe0f11c65fc92
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-04-18 18:54:18 -0500

    Remove custom svg class because I'm stupid and didn't realize I could just use img

--- src/webtk/components/image.ts
+++ src/webtk/components/image.ts
@@ -16,21 +16,3 @@ export class Image extends Widget {
 		this.element.appendChild(this.image);
 	}
 }
-
-export class SVGImage extends Widget {
-	image;
-
-	constructor(innerContent: string, width: number, height: number, stroke: string, fill: string) {
-		super();
-		this.image = document.createElement('svg');
-		this.image.innerHTML = innerContent;
-		this.image.width  = width;
-		this.image.height = height;
-		this.image.setAttribute('stroke', stroke);
-		this.image.setAttribute('fill', fill);
-		this.setWidth(width);
-		this.setHeight(height);
-
-		this.element.appendChild(this.image);
-	}
-}
